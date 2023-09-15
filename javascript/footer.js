d01729a4a16f445e1c987ed09c0950bf251e83d4
IagoGondim <iago-gondim@gotmail.com>
2023-09-15 09:11:51 -0300

    add footer.js e nav.js

---
+++ javascript/footer.js
@@ -0,0 +1,8 @@
+document.write("<footer>");
+document.write("<div class='footerContent'>");
+document.write("<p>&copy; 2023 Meu Site - Todos os direitos reservados</p>");
+document.write(" <p>Brasil</p>");
+document.write("<p>Autor: Iago Gondim</p>");
+document.write("<p>Email: iagogondim200@gmail.com</p>");
+document.write("</div>");
+document.write("</footer>");
---
+++ javascript/nav.js
@@ -0,0 +1,9 @@
+document.write("<header>");
+document.write("<a href='../pages/index.html'><img src='../img/java.svg' alt='' width='64px' /></a>");
+document.write("<nav><ul>");
+document.write("<li><a href='../pages/verificaIdade.html'>Verificar Idade</a></li>");
+document.write("<li><a href='../pages/verificaAluno.html'>Verificar Aluno</a></li>");
+document.write("<li><a href='../pages/verificaIMC.html'>Verificar IMC</a></li>");
+document.write("<li><a href='../pages/verificaAumento.html'>Verificar Aumento</a></li>");
+document.write("</ul></nv>");
+document.write("</header>");
